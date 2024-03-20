0eef8530fa4513155d0c8df21b4e3fa25f89158a
jjyj5264 <jjyj5264@gmail.com>
2024-03-20 14:45:41 +0900

    Added two string type function.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 #include <string>
 using namespace std;
 
+/**
+ * Returns subtraction of two BigInteger(std:string type).
+*/
+std::string sum(std::string first, std::string second) {
+
+}
+
+/**
+ * Returns subtraction of two BigInteger(std:string type).
+*/
+std::string sub(std::string first, std::string second) {
+    
+}
+
 int main() {
     string first = "";
     string second = "";
@@ -11,4 +25,7 @@ int main() {
     cin >> first;
     cout << "Second Number: ";
     cin >> second;
+
+    cout << "Sum of two numbers: " << sum(first, second) << endl;
+    cout << "Subtract of two numbers: " << second << endl;
 }

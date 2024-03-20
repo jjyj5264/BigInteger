71dbeaa740b4b94532bbf4ad92359153fb0d3a8d
jjyj5264 <jjyj5264@gmail.com>
2024-03-20 14:34:04 +0900

    Created main.cpp and added some basic logics for BigInteger Adder and Subtractor.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() {
+    string first = "";
+    string second = "";
+    
+    cout << "First Number: ";
+    cin >> first;
+    cout << "Second Number: ";
+    cin >> second;
+}
